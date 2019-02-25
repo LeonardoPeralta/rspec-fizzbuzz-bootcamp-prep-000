48f528088f2ed0af2c8855a60a7f3edf5c53459f
Leonardo Peralta <leo.pm15@gmail.com>
2019-02-25 05:32:47 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,12 +3,13 @@
 
 
 def fizzbuzz(num)
-  if num % 3 == 0
-    "Fizz"
+  if num % 3== 0 && num % 5 == 0
+    "FizzBuzz"
   elsif num % 5 == 0
     "Buzz"
-  elsif num % 3== 0 && num % 5 == 0
-    "FizzBuzz"
+  elsif 
+    num % 3 == 0
+    "Fizz"
   end
 
 end
